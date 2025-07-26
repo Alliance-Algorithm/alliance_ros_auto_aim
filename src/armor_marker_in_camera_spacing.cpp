ac598c96530f5784715eec8aa4f9d88d575a47b9
alray <1780284652@qq.com>
2025-07-26 23:36:50 +0800

    修复坐标系

--- src/armor_marker_in_camera_spacing.cpp
+++ src/armor_marker_in_camera_spacing.cpp
@@ -8,87 +8,89 @@ namespace world_exe::ros {
 
 class ArmorMarkerInCameraSpacing::Impl {
 public:
-    Impl() {
-        color_.r = 0.0;
-        color_.g = 1.0;
-        color_.b = 0.0;
-        color_.a = 1.0;
+  Impl() {
+    color_.r = 0.0;
+    color_.g = 1.0;
+    color_.b = 0.0;
+    color_.a = 1.0;
 
-        small_armor_.x = 0.135;
-        small_armor_.y = 0.020;
-        small_armor_.z = 0.125;
+    small_armor_.x = 0.020;
+    small_armor_.y = 0.135;
+    small_armor_.z = 0.125;
 
-        large_armor_.x = 0.230;
-        large_armor_.y = 0.020;
-        large_armor_.z = 0.127;
+    large_armor_.y = 0.230;
+    large_armor_.x = 0.020;
+    large_armor_.z = 0.127;
+  }
+  ~Impl() = default;
+
+  void set_frame_id(const std::string &frame_id) { frame_id_ = frame_id; }
+  std::string frame_id() { return frame_id_; }
+
+  void set_color(float r, float g, float b, float a) {
+    color_.r = r;
+    color_.g = g;
+    color_.b = b;
+    color_.a = a;
+  }
+
+  void set_time_stamp(const rclcpp::Time &time) { time_stamp_ = time; }
+
+  visualization_msgs::msg::MarkerArray
+  generate(const std::vector<data::ArmorCameraSpacing> &armors_data) {
+    if (frame_id_.empty()) {
+      throw std::runtime_error("ArmorMarker frame_id not setting!");
     }
-    ~Impl() = default;
 
-    void set_frame_id(const std::string& frame_id) { frame_id_ = frame_id; }
-    std::string frame_id() { return frame_id_; }
+    visualization_msgs::msg::MarkerArray marker_array;
 
-    void set_color(float r, float g, float b, float a) {
-        color_.r = r;
-        color_.g = g;
-        color_.b = b;
-        color_.a = a;
+    int marker_count = 0;
+    for (const auto &armor : armors_data) {
+      using namespace enumeration;
+      visualization_msgs::msg::Marker marker;
+      if (armor.id == ArmorIdFlag::Unknow && armor.id == ArmorIdFlag::None) {
+        continue;
+      } else if (armor.id == ArmorIdFlag::Hero &&
+                 armor.id == ArmorIdFlag::Base) {
+        marker.set__scale(large_armor_);
+      } else {
+        marker.set__scale(small_armor_);
+      }
+      marker.set__color(color_);
+
+      if (time_stamp_.nanoseconds() == 0) {
+        marker.header.stamp = rclcpp::Clock().now();
+      } else {
+        marker.header.stamp = time_stamp_;
+      }
+      marker.header.frame_id = frame_id_;
+
+      marker.ns = "armor";
+      marker.id = marker_count++;
+      marker.type = visualization_msgs::msg::Marker::CUBE;
+      marker.action = visualization_msgs::msg::Marker::ADD;
+
+      marker.pose.position.x = armor.position.x();
+      marker.pose.position.y = armor.position.y();
+      marker.pose.position.z = armor.position.z();
+
+      marker.pose.orientation.x = armor.orientation.x();
+      marker.pose.orientation.y = armor.orientation.y();
+      marker.pose.orientation.z = armor.orientation.z();
+      marker.pose.orientation.w = armor.orientation.w();
+      marker.lifetime = rclcpp::Duration::from_seconds(0.1);
+
+      marker_array.markers.push_back(marker);
     }
-
-    void set_time_stamp(const rclcpp::Time& time) { time_stamp_ = time; }
-
-    visualization_msgs::msg::MarkerArray generate(const std::vector<data::ArmorCameraSpacing>& armors_data) {
-        if (frame_id_.empty()) {
-            throw std::runtime_error("ArmorMarker frame_id not setting!");
-        }
-
-        visualization_msgs::msg::MarkerArray marker_array;
-
-        int marker_count = 0;
-        for (const auto& armor : armors_data) {
-            using namespace enumeration;
-            visualization_msgs::msg::Marker marker;
-            if (armor.id == ArmorIdFlag::Unknow && armor.id == ArmorIdFlag::None) {
-                continue;
-            } else if (armor.id == ArmorIdFlag::Hero && armor.id == ArmorIdFlag::Base) {
-                marker.set__scale(large_armor_);
-            } else {
-                marker.set__scale(small_armor_);
-            }
-            marker.set__color(color_);
-
-            if (time_stamp_.nanoseconds() == 0) {
-                marker.header.stamp = rclcpp::Clock().now();
-            } else {
-                marker.header.stamp = time_stamp_;
-            }
-            marker.header.frame_id = frame_id_;
-
-            marker.ns     = "armor";
-            marker.id     = marker_count++;
-            marker.type   = visualization_msgs::msg::Marker::CUBE;
-            marker.action = visualization_msgs::msg::Marker::ADD;
-
-            marker.pose.position.x = armor.position.x();
-            marker.pose.position.y = armor.position.y();
-            marker.pose.position.z = armor.position.z();
-
-            marker.pose.orientation.x = armor.orientation.x();
-            marker.pose.orientation.y = armor.orientation.y();
-            marker.pose.orientation.z = armor.orientation.z();
-            marker.pose.orientation.w = armor.orientation.w();
-            marker.lifetime           = rclcpp::Duration::from_seconds(0.1);
-
-            marker_array.markers.push_back(marker);
-        }
-        return marker_array;
-    }
+    return marker_array;
+  }
 
 private:
-    std::string frame_id_;
-    std_msgs::msg::ColorRGBA color_;
-    rclcpp::Time time_stamp_;
-    geometry_msgs::msg::Vector3 small_armor_;
-    geometry_msgs::msg::Vector3 large_armor_;
+  std::string frame_id_;
+  std_msgs::msg::ColorRGBA color_;
+  rclcpp::Time time_stamp_;
+  geometry_msgs::msg::Vector3 small_armor_;
+  geometry_msgs::msg::Vector3 large_armor_;
 };
 
 ArmorMarkerInCameraSpacing::ArmorMarkerInCameraSpacing()
@@ -96,17 +98,25 @@ ArmorMarkerInCameraSpacing::ArmorMarkerInCameraSpacing()
 
 ArmorMarkerInCameraSpacing::~ArmorMarkerInCameraSpacing() = default;
 
-void ArmorMarkerInCameraSpacing::set_frame_id(const std::string& frame_id) { pimpl_->set_frame_id(frame_id); }
+void ArmorMarkerInCameraSpacing::set_frame_id(const std::string &frame_id) {
+  pimpl_->set_frame_id(frame_id);
+}
 
-std::string ArmorMarkerInCameraSpacing::frame_id() { return pimpl_->frame_id(); }
+std::string ArmorMarkerInCameraSpacing::frame_id() {
+  return pimpl_->frame_id();
+}
 
-void ArmorMarkerInCameraSpacing::set_color(float r, float g, float b, float a) { pimpl_->set_color(r, g, b, a); }
+void ArmorMarkerInCameraSpacing::set_color(float r, float g, float b, float a) {
+  pimpl_->set_color(r, g, b, a);
+}
 
-void ArmorMarkerInCameraSpacing::set_time_stamp(const rclcpp::Time& time) { pimpl_->set_time_stamp(time); }
+void ArmorMarkerInCameraSpacing::set_time_stamp(const rclcpp::Time &time) {
+  pimpl_->set_time_stamp(time);
+}
 
-visualization_msgs::msg::MarkerArray
-    ArmorMarkerInCameraSpacing::generate(const std::vector<data::ArmorCameraSpacing>& armors_data) {
-    return pimpl_->generate(armors_data);
+visualization_msgs::msg::MarkerArray ArmorMarkerInCameraSpacing::generate(
+    const std::vector<data::ArmorCameraSpacing> &armors_data) {
+  return pimpl_->generate(armors_data);
 }
 
 } // namespace world_exe::ros
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,6 +1,10 @@
 
 #include "./tests.hpp"
+#include "enum/system_version.hpp"
+#include "system_factory.hpp"
 
 int main(int argc, const char *const *argv) {
+  world_exe::core::SystemFactory::Build(
+      world_exe::enumeration::SystemVersion::V1);
   world_exetest::ros::test::test_armor_3d_camera_in_3d_view(argc, argv);
 }
