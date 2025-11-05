c1bb0bb4c1f87c0735585924e3c55e11a45cc13d
heyeuu <2829004293@qq.com>
2025-11-05 22:00:31 +0800

    test:test identifier ,solver

--- tests/image_and_data.hpp
+++ tests/image_and_data.hpp
@@ -1,49 +1,107 @@
+#include "armor_marker.hpp"
 #include "core/event_bus.hpp"
 #include "data/mat_stamped.hpp"
+#include "interfaces/armor_in_camera.hpp"
+#include "parameters/params_system_v1.hpp"
 #include "std_msgs/msg/u_int8_multi_array.hpp"
 #include "sync_data_processor.hpp"
 #include "utils/mat_triple_buffer.hpp"
 #include "utils/time_stamp.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 #include <functional>
+#include <iostream>
 #include <memory>
+#include <rclcpp/logging.hpp>
 #include <rclcpp/node.hpp>
+#include <rclcpp/publisher.hpp>
 #include <rclcpp/subscription.hpp>
 #include <thread>
 namespace alliance_auto_aim::ros::bulldup {
 
-    class DataNode: public rclcpp::Node{
-        public:
-            using Data = world_exe::ros::SyncData_Feb_TimeCameraGimbal_8byteAlignas;
-            DataNode(std::string image_event, std::string sync_data_event, std::function<cv::Mat()> func) : 
-                rclcpp::Node("image_and_data", "/alliacne_auto_aim")
-               ,buffer_(world_exe::util::time_stamp::SteadyClock{})    
-            {
-                sync_data_sub_ = create_subscription<std_msgs::msg::UInt8MultiArray>(
-                    "/alliacne_auto_aim/camera/sync_data", 10, 
-                    [&](std_msgs::msg::UInt8MultiArray::UniquePtr data){
-                        auto raw = *reinterpret_cast<const Data*>(data->data.data());
-                        auto decode =  world_exe::ros::sync_data_process(raw);
-                        world_exe::core::EventBus::Publish<world_exe::data::CameraGimbalMuzzleSyncData&>(sync_data_event, decode);
-                    });
+class DataNode : public rclcpp::Node {
+public:
+  using Data = world_exe::ros::SyncData_Feb_TimeCameraGimbal_8byteAlignas;
+  DataNode(const std::string &image_event, const std::string &sync_data_event,
+           std::function<cv::Mat()> func)
+      : rclcpp::Node("image_and_data", "/alliacne_auto_aim"),
+        buffer_(world_exe::util::time_stamp::SteadyClock{}) {
+    sync_data_sub_ = create_subscription<std_msgs::msg::UInt8MultiArray>(
+        "/alliacne_auto_aim/camera/sync_data", 10,
+        [&](std_msgs::msg::UInt8MultiArray::UniquePtr data) {
+          auto raw = *reinterpret_cast<const Data *>(data->data.data());
+          auto decode = world_exe::ros::sync_data_process(raw);
+          world_exe::core::EventBus::Publish<
+              world_exe::data::CameraGimbalMuzzleSyncData &>(sync_data_event,
+                                                             decode);
+        });
 
-                capture_thread = std::thread([&func,this](){
-                    while(true)
-                    {
-                        buffer_.set(func());
-                    }
-                });
-                publish_thread = std::thread([&image_event, this](){
-                    while(true)
-                    {
-                        world_exe::core::EventBus::Publish<world_exe::data::MatStamped>(image_event, buffer_.get()->get());
-                    }
-                });
-            }
-            ~DataNode() = default;
-        private:
-            rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr sync_data_sub_;
-            world_exe::util::memory::MatTripleBuffer<world_exe::util::time_stamp::SteadyClock> buffer_;
-            std::thread capture_thread;
-            std::thread publish_thread;
-    };
-}
+    publisher_pnp_ = create_publisher<visualization_msgs::msg::MarkerArray>(
+        "/alliacne_auto_aim/armor_pnp", 10);
+
+    publisher_gimbal_ = create_publisher<visualization_msgs::msg::MarkerArray>(
+        "/alliacne_auto_aim/armor_in_gimbal", 10);
+
+    capture_thread = std::thread([&func, this]() {
+      while (true) {
+        buffer_.set(func());
+      }
+    });
+
+    publish_thread = std::thread([image_event, this]() {
+      while (true) {
+        auto mat = buffer_.get();
+        if (!mat.has_value() || mat->get().mat.empty())
+          continue;
+
+        world_exe::core::EventBus::Publish<world_exe::data::MatStamped>(
+            image_event, mat->get());
+      }
+    });
+
+    world_exe::core::EventBus::Subscript<
+        std::shared_ptr<world_exe::interfaces::IArmorInCamera>>(
+        world_exe::parameters::ParamsForSystemV1::armors_in_camera_pnp_event,
+        [&](const std::shared_ptr<world_exe::interfaces::IArmorInCamera>
+        &data)
+            -> void {
+          visualization_msgs::msg::MarkerArray msg{};
+          if (data == nullptr) {
+            return;
+          }
+          world_exe::ros::ArmorMarkerGenerator::generate_all(
+              *data, "camera_link", msg);
+          publisher_pnp_->publish(msg);
+        });
+
+    world_exe::core::EventBus::Subscript<
+        std ::shared_ptr<world_exe::interfaces ::IArmorInGimbalControl>>(
+        world_exe::parameters::ParamsForSystemV1::tracker_current_armors_event,
+        [&](std ::shared_ptr<world_exe::interfaces ::IArmorInGimbalControl>
+                data) -> void {
+          visualization_msgs::msg::MarkerArray msg{};
+          if (data == nullptr) {
+            return;
+          }
+          world_exe::ros::ArmorMarkerGenerator::generate_all(
+              *data, "gimbal_link", msg);
+
+          publisher_gimbal_->publish(msg);
+        });
+  }
+  ~DataNode() = default;
+
+private:
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
+      publisher_pnp_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
+      publisher_gimbal_;
+
+  rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr
+      sync_data_sub_;
+  world_exe::util::memory::MatTripleBuffer<
+      world_exe::util::time_stamp::SteadyClock>
+      buffer_;
+  std::thread capture_thread;
+  std::thread publish_thread;
+};
+} // namespace alliance_auto_aim::ros::bulldup
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,14 +1,69 @@
 
-#include "core/system_factory.hpp"
 #include "./image_and_data.hpp"
+
+#include "core/system_factory.hpp"
+#include "interfaces/armor_in_camera.hpp"
+#include "parameters/params_system_v1.hpp"
+#include "parameters/profile.hpp"
+#include <hikcamera/capturer.hpp>
 #include <memory>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui.hpp>
+#include <print>
+
 #include <rclcpp/executors.hpp>
 #include <rclcpp/utilities.hpp>
 
 int main(int argc, const char *const *argv) {
+  world_exe::parameters::HikCameraProfile::set_width_height(1440, 720);
+
+
   rclcpp::init(argc, argv);
-  world_exe::core::SystemFactory::Build(world_exe::enumeration::SystemVersion::V2);
-  auto node = std::make_shared<alliance_auto_aim::ros::bulldup::DataNode>("1", "2",[](){return cv::Mat();});
+  world_exe::core::SystemFactory::Build(
+      world_exe::enumeration::SystemVersion::V2Debug);
+
+  auto camera = hikcamera::Camera{};
+  auto config = hikcamera::Config{};
+  config.framerate = 500;
+  config.timeout_ms = 2'000;
+  config.exposure_us = 800;
+  config.fixed_framerate = true;
+
+  if (auto ret = camera.initialize(config); !ret) {
+    std::println("Failed: {}", ret.error());
+  }
+
+  // world_exe::util::memory::MatTripleBuffer buffer{[] {
+  //   return world_exe::data::TimeStamp{
+  //       std::chrono::steady_clock::now().time_since_epoch()};
+  // }};
+  auto mat = cv::Mat();
+  std::thread thread_capture{[&] {
+    while (true) {
+      if (auto ret = camera.read_image()) {
+        mat = ret.value();
+        // buffer.set(mat);
+
+      } else {
+        std::println("Failed: {}", ret.error());
+      }
+    }
+  }};
+
+  auto node = std::make_shared<alliance_auto_aim::ros::bulldup::DataNode>(
+      world_exe::parameters::ParamsForSystemV1::raw_image_event,
+      world_exe::parameters::ParamsForSystemV1::camera_capture_transforms,
+      [&mat]() -> cv::Mat {
+        return mat;
+        // auto optional = buffer.get();
+        // if (optional.has_value()) {
+        //   auto &mat = optional.value().get().mat;
+        //   if (!mat.empty())
+        //     return mat;
+        // }
+        // return cv::Mat();
+      });
+
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
